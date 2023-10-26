056334372860886e0dc057a9f9cfdf7e56371ddd
terence <terenceshawn18@outlook.com>
2023-10-26 10:22:48 +0300

    completed

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+
+  function hasTargetSum(array, target) {
+    // Write your algorithm here
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[i] + array[j] === target) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  hasTargetSum([3, 8, 12, 4, 11, 7], 10);
+
 
 /* 
   Write the Big O time complexity of your function here
